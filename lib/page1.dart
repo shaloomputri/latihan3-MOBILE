61c21a3733a4a6fa5d22ee1aabb6fd49c0860c33
shaloomputri <lummmarhass>
2024-08-20 12:05:01 +0700

    perpindahan halaman

---
+++ lib/page1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/page2');
+          },
+          child: Text ('pindah ke halaman 2')),
+      ),
+    );
+  }
+}
